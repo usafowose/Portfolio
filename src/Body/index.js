5fb16f8d92fbc70848143e32d5f92a3a29168732
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-11-24 23:55:08 -0500

    Added ParentCompont for Body and Light Styling

---
+++ src/Body/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BodyLeft from './Body_Left'
+import BodyRight from './Body_Right'
+
+
+const Body = (props) => {
+    return (
+        <div className='container-fluid border border-danger col-10 rounded' style={{ minHeight: '60rem', marginTop: 20, backgroundColor: '', overflow: 'auto', padding:15}}>
+            <BodyLeft />
+            <BodyRight/>
+        </div>
+    )
+}
+
+export default Body;
