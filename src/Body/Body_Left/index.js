6636632ad80f0bc016409113952cb869718ccd9d
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-11-25 00:00:39 -0500

    Small styling changes due to div movement on heroku build

--- src/Body/Body_Left/index.js
+++ src/Body/Body_Left/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BodyLeft = (props) => {
     return (
-        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', margin: 10, marginRight:13, backgroundColor: 'orange', float:'left', width:'48%', padding:5}}>
+        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', margin: 10, marginRight:13, marginLeft: 11, backgroundColor: 'orange', float:'left', width:'48%', padding:5}}>
             <p>Hello</p>
             <p>Hello</p>
             <p>Hello</p>
--- src/Body/Body_Right/index.js
+++ src/Body/Body_Right/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BodyRight = (props) => {
     return (
-        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', float: 'left', backgroundColor: 'orange', margin:10, marginLeft: 13, width:'48%', padding: 5}}>
+        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', float: 'left', backgroundColor: 'orange', margin:10, marginLeft: 12, width:'48%', padding: 5}}>
             <p>Hello</p>
             <p>Hello</p>
             <p>Hello</p>
