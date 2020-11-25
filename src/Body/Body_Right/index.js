ebfc4a1995a02bd7c6b4d34cd94705d5d3b6a2d5
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-11-24 23:54:38 -0500

    Added RIght Side Body Par.Compntn and Inline Style

---
+++ src/Body/Body_Right/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const BodyRight = (props) => {
+    return (
+        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', float: 'left', backgroundColor: 'orange', margin:10, marginLeft: 13, width:'48%', padding: 5}}>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+
+        </div>
+    )
+}
+
+export default BodyRight;
