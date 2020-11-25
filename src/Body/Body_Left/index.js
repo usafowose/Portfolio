6abac423ab4b7e8a37bb96e6da9ee13017ecfac3
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-11-24 23:53:50 -0500

    Added Left Side of Body Div (Parent Component)

---
+++ src/Body/Body_Left/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const BodyLeft = (props) => {
+    return (
+        <div className='container-fluid border border-success rounded' style={{ minHeight: '25', margin: 10, marginRight:13, backgroundColor: 'orange', float:'left', width:'48%', padding:5}}>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default BodyLeft;
