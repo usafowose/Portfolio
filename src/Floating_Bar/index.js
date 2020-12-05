ea0d33507f891079802396865de4dabd7dc55a66
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-12-05 16:43:29 -0500

    Replaced fixed profile image with absolute relative to container

--- src/Floating_Bar/index.js
+++ src/Floating_Bar/index.js
@@ -1,41 +1,52 @@
 import React from 'react';
+import Background from './../assets/images/glow-wave-system.jpg'
 
 
 const FloatingBarStyles = {
   containerClass: {
-    marginTop: 5
+    marginTop: 10,
+    opacity: 1.0,
+    overflow: 'hidden'
   },
-  navLink: {
-    color: 'grey',
+  anyText: {
+    color: 'white',
     fontFamily: 'sans-serif'
+  },
+  cardMargin: {
+    marginRight: 10
+  },
+  profileImageDiv: {
+    width: "15rem",
+    backgroundColor: "black",
+    marginLeft: '4rem',
+    marginRight: '13rem',
+    position: 'absolute',
+    overflow: 'hidden',
+    top: '12%' //adjusting image to center of div (based on heigth of image, tho, so as div changes/expands, this must be adjusted to stay centered)
   }
 }
 
 const FloatingBar = (props) => {
   return (
     <div className='container-fluid col-11' style={FloatingBarStyles.containerClass}>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-secodary rounded-lg">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className="navbar navbar-expand-lg justify-content-start navbar-dark rounded">
+        <ul className="navbar-nav">
+          <div className="card " style={FloatingBarStyles.profileImageDiv}>
+            <li className="nav-item" style={{ overflow: 'hidden' }}>
+              <img className="card-img-top" src={Background} alt="Me, Andrew" style={{ minHeight: 160 }} />
+            </li>
+          </div>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#" style={FloatingBarStyles.navLink}>Home <span className="sr-only">(current)</span></a>
+          <div className='row' style={{ marginLeft: '30rem', textAlign: 'center' }}>
+            <li className='col-10' style={{marginTop:15}}>
+              <p style={FloatingBarStyles.anyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in tellus pulvinar, fermentum lorem sed, venenatis velit. Morbi varius pulvinar vulputate. Sed vulputate ullamcorper dolor eu auctor. Integer consequat nulla at augue molestie dictum. Vestibulum sit amet ante id nunc commodo egestas vel at magna. Aliquam et consequat felis, a convallis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed venenatis augue at nisl tincidunt, et facilisis erat aliquam. Vivamus eros enim, pharetra eu dui blandit, sagittis volutpat justo. Sed vehicula nunc at ipsum pretium consectetur. Quisque molestie orci vel est aliquet posuere. Nullam. </p>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#" style={FloatingBarStyles.navLink}>Link</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style={FloatingBarStyles.navLink}>Disabled</a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
+          </div>
+
+          {/* <li className="nav-item">
+            <a className="nav-link" href="#" style={FloatingBarStyles.anyText}>Disabled</a>
+          </li> */}
+        </ul>
       </nav>
     </div>
   )
