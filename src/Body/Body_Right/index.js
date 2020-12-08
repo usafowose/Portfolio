ebf278b748648c110be51dba17fb09ace2416b5d
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-12-08 00:06:47 -0500

    Bringing in props to parent-most component

--- src/Body/Body_Right/index.js
+++ src/Body/Body_Right/index.js
@@ -1,33 +1,38 @@
 import React from 'react'
-import HorizontalDiv from './../Body_Left/horizontal_cards'
+import HorizontalDiv from '../Body_Left/horizontalDiv'
+// import styles from './../index.css'
 
-const bodyRightStyle = {
-    minHeight: '25',
-    float: 'left',
-    backgroundColor: 'grey',
-    margin: 10,
-    marginLeft: 13,
-    width: '48%',
-    overflow: 'auto'
-}
+const bodyRightStyle = [
+    {
+        minHeight: '25',
+        float: 'left',
+        // backgroundColor: 'grey',
+        margin: 10,
+        marginLeft: 13,
+        width: '48%',
+        overflow: 'auto'
+    },
+    {
+        color: 'white'
+    }]
 
 const BodyRight = (props) => {
     return (
-        <div className='container-fluid border border-success rounded' style={bodyRightStyle}>
+        <div className='container-fluid border border-success rounded' style={bodyRightStyle[0]}>
             <HorizontalDiv />
-            <p>Hello</p>
-            <p>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
             <HorizontalDiv />
-            <p>Hello</p>
-            <p>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
             <HorizontalDiv />
-            <p>Hello</p>
-            <p>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
             <HorizontalDiv />
-            <p>Hello</p>
-            <p>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
             <HorizontalDiv />
-            <p>Hello</p>
+            <p style={bodyRightStyle[1]}>Hello</p>
         </div>
     )
 }
--- src/Body/index.js
+++ src/Body/index.js
@@ -7,13 +7,13 @@ const bodyContainerStyle = {
     marginTop: 20,
     overflow: 'auto',
     padding: 'none', 
-    opacity: 0.95,
-    backgroundColor: 'white'
+    opacity: 0.6,
+    backgroundColor: 'aliceBlue'
 }
 
 const Body = (props) => {
     return (
-        <div className='container-fluid border border-success col-10' style={bodyContainerStyle}>
+        <div className='container-fluid border border-success col-11 rounded' style={bodyContainerStyle}>
             <BodyLeft />
             <BodyRight />
         </div>
